71448742ec67d9925c88af267e182405d74fae8c
Jakub Czesak <jakub.czesak@cbre.com>
2023-07-13 22:37:56 +0200

    Add websocket

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const shortid = require('shortid');
-const db = require('./db');
 const cors = require('cors');
 const path = require('path');
-
-
+const socket = require('socket.io');
+const db = require('./db');
 const app = express();
-app.use(cors());
-app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -27,7 +31,12 @@ app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
 
-
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log(socket.id + ' connected');
 });
