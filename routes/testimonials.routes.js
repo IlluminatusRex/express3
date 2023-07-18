e98886196a237b9ecf7987a20eff67c66bd66d9a
Jakub Czesak <jakub.czesak@cbre.com>
2023-07-18 14:05:17 +0200

    Add MongoDB

--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -1,53 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
+const TestimonialController = require('../controllers/testimonials.controller');
 
-router.route('/').get((req, res) => {
-  res.send(db.testimonials);
-});
+router.get('/', TestimonialController.getAll);
 
-router.route('/random').get((req, res) => {
-  const randomID = db.testimonials[Math.floor(Math.random() * db.testimonials.length)];
-  res.send(randomID);
-});
+router.get('/random', TestimonialController.getRandom);
 
-router.route('/').post((req, res) => {
-  const id = shortid();
-  const { author, text } = req.body;
-  db.testimonials.push({ id, author, text });
-  res.send({ message: 'ok' });
-});
+router.post('/', TestimonialController.add);
 
-router.route('/:id').get((req, res) => {
-  const idSelect = db.testimonials.find((item) => item.id === req.params.id);
-  if (idSelect) {
-    res.send(idSelect);
-  } else {
-    res.status(404).send({ message: 'Not found...' });
-  }
-});
+router.get('/:id', TestimonialController.getById);
 
-router.route('/:id').put((req, res) => {
-  const idSelect = db.testimonials.find((item) => item.id === req.params.id);
-  if (idSelect) {
-    const { author, text } = req.body;
-    idSelect.author = author || idSelect.author;
-    idSelect.text = text || idSelect.text;
-    res.send({ message: 'ok' });
-  } else {
-    res.status(404).send({ message: 'Not found...' });
-  }
-});
+router.put('/:id', TestimonialController.edit);
 
-router.route('/:id').delete((req, res) => {
-  const idSelect = db.testimonials.findIndex((item) => item.id === req.params.id);
-  console.log(idSelect);
-  if (idSelect || idSelect === 0) {
-    db.testimonials.splice(idSelect, 1);
-    res.send({ message: 'ok' });
-  } else {
-    res.status(404).send({ message: 'Not found...' });
-  }
-});
-
+router.delete('/:id', TestimonialController.delete);
 module.exports = router;
